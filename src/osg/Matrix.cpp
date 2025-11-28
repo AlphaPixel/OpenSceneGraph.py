68574eac6080f735c7952dc5e2c1f05e7c3ffa93
Jeremy Moles <cubicool@gmail.com>
2025-11-27 23:41:13 -0500

    Adds Matrix method placeholders

--- src/osg/Matrix.cpp
+++ src/osg/Matrix.cpp
@@ -17,13 +17,22 @@ namespace detail {
 		}
 	}
 
+	template<typename T> struct MatrixTraits;
+
+	template<> struct MatrixTraits<osg::Matrixf> {
+		using Vec3 = osg::Vec3f;
+	};
+
+	template<> struct MatrixTraits<osg::Matrixd> {
+		using Vec3 = osg::Vec3d;
+	};
+
 	template<typename T>
 	auto bind_Matrix(py::module_& m, const char* name) {
 		using value_type = typename T::value_type;
 
 		return py::class_<T>(m, name)
 			.def(py::init<>())
-			// .def(py::init<const std::array<value_type, 16>&>())
 			.def(py::init([](const std::array<value_type, 16>& vals) {
 				return T(vals.data());
 			}))
@@ -33,21 +42,25 @@ namespace detail {
 				value_type, value_type, value_type, value_type,
 				value_type, value_type, value_type, value_type
 			>())
+			.def(py::init<const osg::Quat&>())
 			.def(py::init<const T&>())
-			// init: from Quat
 
-			// compare
-			// operator<
-			// operator==
-			// operator!=
+			// This is called by the operators below; it seems to return < 0 for "less than", equal
+			// to 0 for "equal to", and greater than 0 for "not equal." I think.
+			// TODO: Do we NEED this?
+			.def("compare", &T::compare)
+			.def(py::self < py::self)
+			.def(py::self == py::self)
+			.def(py::self != py::self)
 
-			// element access [][]
+			// Mirrors the OSG method of operator()(row, col) access.
 			.def("__call__", [](T& self, size_t row, size_t col) {
 				assert_indices<4>(row, col);
 
 				return self(static_cast<int>(row), static_cast<int>(col));
 			})
 
+			// A far more Pythonic interface where 2 indices are always specified.
 			.def("__getitem__", [](const T& self, std::pair<size_t, size_t> rc) {
 				auto [row, col] = rc;
 
@@ -70,23 +83,184 @@ namespace detail {
 				});
 			})
 
-			// valid
-			// isNan
+			.def("valid", &T::valid)
+			.def("isNaN", &T::isNaN)
+			.def("isIdentity", &T::isIdentity)
+			.def("makeIdentity", &T::makeIdentity)
+			.def("makeScale", py::overload_cast<const osg::Vec3f&>(&T::makeScale))
+			.def("makeScale", py::overload_cast<const osg::Vec3d&>(&T::makeScale))
+			.def("makeScale", py::overload_cast<value_type, value_type, value_type>(&T::makeScale))
+			.def("makeTranslate", py::overload_cast<const osg::Vec3f&>(&T::makeTranslate))
+			.def("makeTranslate", py::overload_cast<const osg::Vec3d&>(&T::makeTranslate))
+			.def("makeTranslate", py::overload_cast<
+				value_type,
+				value_type,
+				value_type
+			>(&T::makeTranslate))
+			.def("makeRotate", py::overload_cast<
+				const osg::Vec3f&,
+				const osg::Vec3f&
+			>(&T::makeRotate))
+			.def("makeRotate", py::overload_cast<
+				const osg::Vec3d&,
+				const osg::Vec3d&
+			>(&T::makeRotate))
+			.def("makeRotate", py::overload_cast<value_type, const osg::Vec3f&>(&T::makeRotate))
+			.def("makeRotate", py::overload_cast<value_type, const osg::Vec3d&>(&T::makeRotate))
+			.def("makeRotate", py::overload_cast<
+				value_type,
+				value_type,
+				value_type,
+				value_type
+			>(&T::makeRotate))
+			.def("makeRotate", py::overload_cast<const osg::Quat&>(&T::makeRotate))
+			.def("makeRotate", py::overload_cast<
+				value_type, const osg::Vec3f&,
+				value_type, const osg::Vec3f&,
+				value_type, const osg::Vec3f&
+			>(&T::makeRotate))
+			.def("makeRotate", py::overload_cast<
+				value_type, const osg::Vec3d&,
+				value_type, const osg::Vec3d&,
+				value_type, const osg::Vec3d&
+			>(&T::makeRotate))
+			.def("makeOrtho", py::overload_cast<
+				double, double,
+				double, double,
+				double, double
+			>(&T::makeOrtho))
+			/* .def("getOrtho", py::overload_cast<
+				value_type&, value_type&,
+				value_type&, value_type&,
+				value_type&, value_type&
+			>(&T::getOrtho)) */
 
-			// make{Identity,Scale,Translate,Rotate
-			// decompose
+#if 0
+			void makeOrtho(double left,   double right,
+				double bottom, double top,
+				double zNear,  double zFar);
 
-			// .def_static("identity"
-			// .def_static("scale"
-			// .def_static("translate"
-			// .def_static("rotate"
-			// .def_static("inverse"
-			// .def_static("orthoNormal"
-			// .def_static("ortho"
-			// .def_static("ortho2D"
-			// .def_static("frustum"
-			// .def_static("perspective"
-			// .def_static("lookAt"
+			bool getOrtho(double& left,   double& right,
+				double& bottom, double& top,
+				double& zNear,  double& zFar) const;
+
+			bool getOrtho(float& left,   float& right,
+				float& bottom, float& top,
+				float& zNear,  float& zFar) const;
+
+			inline void makeOrtho2D(double left,   double right,
+				double bottom, double top)
+
+			void makeFrustum(double left,   double right,
+				double bottom, double top,
+				double zNear,  double zFar);
+
+			bool getFrustum(double& left,   double& right,
+				double& bottom, double& top,
+				double& zNear,  double& zFar) const;
+
+			bool getFrustum(float& left,   float& right,
+				float& bottom, float& top,
+				float& zNear,  float& zFar) const;
+
+			void makePerspective(double fovy,  double aspectRatio,
+				double zNear, double zFar);
+
+			bool getPerspective(double& fovy,  double& aspectRatio,
+				double& zNear, double& zFar) const;
+
+			bool getPerspective(float& fovy,  float& aspectRatio,
+				float& zNear, float& zFar) const;
+
+			void makeLookAt(const Vec3d& eye,const Vec3d& center,const Vec3d& up);
+
+			void getLookAt(Vec3f& eye,Vec3f& center,Vec3f& up,
+				value_type lookDistance=1.0f) const;
+
+			void getLookAt(Vec3d& eye,Vec3d& center,Vec3d& up,
+				value_type lookDistance=1.0f) const;
+
+			inline bool invert( const Matrixf& rhs)
+
+			bool invert_4x3( const Matrixf& rhs);
+
+			bool invert_4x4( const Matrixf& rhs);
+
+			bool transpose(const Matrixf&rhs);
+
+			bool transpose3x3(const Matrixf&rhs);
+
+			void orthoNormalize(const Matrixf& rhs);
+#endif
+
+			/* .def("decompose", py::overload_cast<
+				osg::Vec3f&,
+				osg::Quat&,
+				osg::Vec3f&,
+				osg::Quat&
+			>(&T::decompose, py::const_))
+			.def("decompose", py::overload_cast<
+				osg::Vec3d&,
+				osg::Quat&,
+				osg::Vec3d&,
+				osg::Quat&
+			>(&T::decompose, py::const_)) */
+
+			.def("decompose", [](const T& self) {
+				typename MatrixTraits<T>::Vec3 translation, scale;
+				osg::Quat rotation, so;
+
+				self.decompose(translation, rotation, scale, so);
+
+				return py::make_tuple(translation, rotation, scale, so);
+			})
+
+			.def_static("identity", &T::identity)
+			.def_static("scale", py::overload_cast<const osg::Vec3f&>(&T::scale))
+			.def_static("scale", py::overload_cast<const osg::Vec3d&>(&T::scale))
+			.def_static("scale", py::overload_cast<value_type, value_type, value_type>(&T::scale))
+			.def_static("translate", py::overload_cast<const osg::Vec3f&>(&T::translate))
+			.def_static("translate", py::overload_cast<const osg::Vec3d&>(&T::translate))
+			.def_static("translate", py::overload_cast<
+				value_type,
+				value_type,
+				value_type
+			>(&T::translate))
+			.def_static("rotate", py::overload_cast<
+				const osg::Vec3f&,
+				const osg::Vec3f&
+			>(&T::rotate))
+			.def_static("rotate", py::overload_cast<
+				const osg::Vec3d&,
+				const osg::Vec3d&
+			>(&T::rotate))
+			.def_static("rotate", py::overload_cast<value_type, const osg::Vec3f&>(&T::rotate))
+			.def_static("rotate", py::overload_cast<value_type, const osg::Vec3d&>(&T::rotate))
+			.def_static("rotate", py::overload_cast<
+				value_type,
+				value_type,
+				value_type,
+				value_type
+			>(&T::rotate))
+			.def_static("rotate", py::overload_cast<const osg::Quat&>(&T::rotate))
+			.def_static("rotate", py::overload_cast<
+				value_type, const osg::Vec3f&,
+				value_type, const osg::Vec3f&,
+				value_type, const osg::Vec3f&
+			>(&T::rotate))
+			.def_static("rotate", py::overload_cast<
+				value_type, const osg::Vec3d&,
+				value_type, const osg::Vec3d&,
+				value_type, const osg::Vec3d&
+			>(&T::rotate))
+			.def_static("inverse", &T::inverse)
+			.def_static("orthoNormal", &T::orthoNormal)
+
+			// .def_static("ortho", &T::ortho)
+			// .def_static("ortho2D", &T::ortho2D)
+			// .def_static("frustum", &T::frustum)
+			// .def_static("perspective", &T::perspective)
+			// .def_static("lookAt", &T::lookAt)
 		;
 	}
 }
