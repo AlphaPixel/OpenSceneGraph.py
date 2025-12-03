ef17ac3bde4e13b885ab1ffead3780d2b975343d
Jeremy Moles <cubicool@gmail.com>
2025-12-03 08:25:51 -0500

    Adds placeholder Geometry object

---
+++ src/osg/Geometry.cpp
@@ -0,0 +1,24 @@
+#include "../osg.hpp"
+
+PYOSG_DISABLE_WARNINGS
+
+#include <osg/Geometry>
+
+PYOSG_ENABLE_WARNINGS
+
+namespace pyosg {
+
+namespace detail {
+	/* template<>
+	void kwargs_init(osg::Object& self, const py::kwargs& kwargs) {
+		if(kwargs.contains("name")) self.setName(kwargs["name"].cast<std::string>());
+	} */
+}
+
+void bind_Geometry(py::module_& m) {
+	py::class_<osg::Geometry, osg::Drawable, osg::ref_ptr<osg::Geometry>>(m, "Geometry")
+		.def(py::init<>())
+	;
+}
+
+}
