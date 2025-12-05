9bf262c88bf084d732e56f5a7e106c4b91e81cf7
Jeremy Moles <cubicool@gmail.com>
2025-12-05 10:48:08 -0500

    Adds 32 and 64 bit float/double numeric constants

--- src/osg.cpp
+++ src/osg.cpp
@@ -1,8 +1,18 @@
 #include "osg.hpp"
 
+#include <limits>
+
 namespace pyosg {
 
 void bind(py::module_& m) {
+	m.attr("F32_MAX") = std::numeric_limits<float>::max();
+	m.attr("F32_MIN") = std::numeric_limits<float>::lowest(); // most negative
+	m.attr("F32_TINY") = std::numeric_limits<float>::min(); // smallest +normal
+
+	m.attr("F64_MAX") = std::numeric_limits<double>::max();
+	m.attr("F64_MIN") = std::numeric_limits<double>::lowest();
+	m.attr("F64_TINY") = std::numeric_limits<double>::min();
+
 	bind_Notify(m);
 	bind_Vec(m);
 	bind_Matrix(m);
