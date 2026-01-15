a8e8cec629785fa34a7f6be04364aeec40c72dc6
Jeremy Moles <cubicool@gmail.com>
2026-01-15 03:28:08 -0500

    Adds additional low-level CPython members to Object
    This patch adds a method for retrieving the exact pointer address
    for any object inheriting from `osg.Referenced`, as well as
    improving the `referenceCount` method to return both the C++
    `referenceCount` value AND the `Py_REFCNT` value as a namedtuple.

--- src/osg/Object.cpp
+++ src/osg/Object.cpp
@@ -16,16 +16,34 @@ namespace detail {
 }
 
 void bind_Object(py::module_& m) {
+	// auto collections = py::module_::import("collections");
+	// m.attr("RefCounts") = collections.attr("namedtuple")("RefCounts", py::make_tuple("cpp", "py"));
+
+	py::object RefCounts = py::module_::import("collections").attr("namedtuple")(
+		"RefCounts",
+		py::make_tuple("cpp", "py")
+	);
+
+	m.attr("RefCounts") = RefCounts;
+
 	py::class_<osg::Referenced, osg::ref_ptr<osg::Referenced>>(m, "Referenced")
-		.def_property_readonly("referenceCount", &osg::Referenced::referenceCount)
 		.def("ref", [](osg::Referenced& self) { return self.ref(); })
 		.def("unref", [](osg::Referenced& self) { return self.unref(); })
+		// .def_property_readonly("referenceCount", &osg::Referenced::referenceCount)
+		// This returns both the `osg::Referenced` value AND the value reported by CPython. Remember
+		// that the value returned by CPython is always +1 greater than you might expect!
+		.def_property_readonly("referenceCount", [RefCounts](py::handle h) {
+			auto& self = h.cast<osg::Referenced&>();
+
+			return RefCounts(self.referenceCount(), h.ref_count());
+		})
+		.def_property_readonly(
+			"addr",
+			[](const osg::Referenced& self) { return reinterpret_cast<uintptr_t>(&self); }
+		);
 	;
 
 	py::class_<osg::Object, osg::Referenced, osg::ref_ptr<osg::Object>>(m, "Object")
-		/* .def_property("name", &osg::Object::getName, [](osg::Object& self, const std::string& name) {
-			self.setName(name);
-		}) */
 		.def_property("name",
 			&osg::Object::getName,
 			py::overload_cast<const char*>(&osg::Object::setName)
