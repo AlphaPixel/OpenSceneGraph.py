f0837910c0e0a7340a5536abcd1916601fb9f6f1
Jeremy Moles <cubicool@gmail.com>
2026-08-01 14:17:20 -0400

    Updates `osg_StateSet.py` to test new `.attributes` property

--- test/osg_StateSet.py
+++ test/osg_StateSet.py
@@ -4,7 +4,7 @@
 
 import pytest
 
-from OpenSceneGraph.osg import StateSet, StateAttribute, Texture2D, Uniform, Vec3f, Matrixf
+from OpenSceneGraph.osg import StateSet, StateAttribute, Program, Texture2D, Uniform, Vec3f, Matrixf
 
 def test_uniforms_append(uniform_init):
 	ss = StateSet()
@@ -92,3 +92,48 @@ def test_texture_attributes_mapping():
 	assert len(ss.textureAttributes) == 1
 	assert ss.textureAttributes.keys() == [1]
 	assert 0 not in ss.textureAttributes
+
+def test_attributes_mapping():
+	ss = StateSet()
+	p = Program(name="p")
+
+	# The subscript key is the attribute's OWN `StateAttribute::Type` -- it's not inferred, so
+	# it must be repeated even though `p` already knows its own type.
+	ss.attributes[StateAttribute.PROGRAM] = p
+
+	assert ss.attributes[StateAttribute.PROGRAM] is p
+	assert len(ss.attributes) == 1
+	assert ss.attributes.keys() == [StateAttribute.PROGRAM]
+	assert StateAttribute.PROGRAM in ss.attributes
+	assert StateAttribute.TEXTURE not in ss.attributes
+
+	del ss.attributes[StateAttribute.PROGRAM]
+
+	assert len(ss.attributes) == 0
+	assert StateAttribute.PROGRAM not in ss.attributes
+
+def test_attributes_tuple_mode():
+	ss = StateSet()
+	p = Program(name="p")
+
+	ss.attributes[StateAttribute.PROGRAM] = (p, StateAttribute.OVERRIDE)
+
+	assert ss.attributes[StateAttribute.PROGRAM] is p
+
+def test_attributes_append_infers_key():
+	ss = StateSet()
+	p = Program(name="p")
+
+	# append()/extend() read the key from `p.type` -- this is the syntax that sidesteps having
+	# to name `StateAttribute.PROGRAM` a second time.
+	ss.attributes.append(p)
+
+	assert ss.attributes[StateAttribute.PROGRAM] is p
+	assert len(ss.attributes) == 1
+
+def test_attributes_key_type_mismatch():
+	ss = StateSet()
+	p = Program(name="p")
+
+	with pytest.raises(ValueError):
+		ss.attributes[StateAttribute.TEXTURE] = p
